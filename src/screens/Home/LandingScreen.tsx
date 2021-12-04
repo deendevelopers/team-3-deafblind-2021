20a952b2d376e13c7d262136367eaafba7c959e6
Yusuf <40564761+yusufcodes@users.noreply.github.com>
2021-12-04 22:00:32 +0000

    Feat: Google Maps API

--- src/screens/Home/LandingScreen.tsx
+++ src/screens/Home/LandingScreen.tsx
@@ -1,9 +1,10 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import { OnboardingScreens } from 'navigation/Navigation.types';
 import { OnboardingScreenParamList } from 'navigation/Navigator';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, Button } from 'react-native';
 import Info from 'icons/Info';
+import { apiCall } from '../../network/GoogleMapsAPI';
 
 export type LandingScreenRoutingProps = StackScreenProps<
   OnboardingScreenParamList,

84208980e2b79e66505edf8a6ff3f21d35eaa55f
Yusuf <40564761+yusufcodes@users.noreply.github.com>
2021-12-05 08:44:42 +0000

    Added: distance away from place & data caching

--- src/network/GoogleMapsAPI.ts
+++ src/network/GoogleMapsAPI.ts
@@ -1,4 +1,5 @@
 const axios = require('axios');
+import { kmToMiles } from '../utils/kmToMiles';
 import { GOOGLE_MAPS_API_KEY } from '../utils/extras';
 import { types } from './GoogleMapsAPI.types';
 
@@ -25,35 +26,75 @@ import { types } from './GoogleMapsAPI.types';
 const lat = '51.510084785550006';
 const lng = '-0.13502325923340425';
 
-export async function apiCall(type: types = types.bank) {
-  const response = await axios.get(
-    `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${lng}&radius=5000&key=${GOOGLE_MAPS_API_KEY}&type=${type}`
-  );
-
-  const transformedData = response.data.results.map(
-    async ({ place_id, name, photos, geometry }) => {
-      const currentLocationAddress = await getAddress(
-        geometry.location.lat,
-        geometry.location.lng
-      );
-
-      return {
-        place_id,
-        name,
-        photos,
-        location: currentLocationAddress,
-      };
-    }
-  );
-
-  const finalData = await Promise.all(transformedData);
-  return finalData;
+interface ILocationData {
+  place_id: string;
+  name: string;
+  photos: object;
+  location: string;
 }
 
-export default async function getAddress(lat: string, lng: string) {
-  const response =
-    await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${GOOGLE_MAPS_API_KEY}
+export async function getPlacesByType(type: types = types.bank) {
+  try {
+    const response = await axios.get(
+      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${lng}&radius=5000&key=${GOOGLE_MAPS_API_KEY}&type=${type}&keyword=${type}`
+    );
+
+    if (response.data.error_message)
+      return console.log(response.data.error_message);
+
+    const transformedData = response.data.results.map(
+      async ({ place_id, name, photos, geometry }, index) => {
+        const currentLocationAddress = await getAddress(
+          geometry.location.lat,
+          geometry.location.lng
+        );
+
+        const currentDistance = await getDistanceToPlace(place_id);
+
+        return {
+          place_id,
+          name,
+          photos,
+          location: currentLocationAddress,
+          distance: (currentDistance / 10).toFixed(2),
+        };
+      }
+    );
+
+    const finalData = await Promise.all(transformedData);
+    return finalData;
+  } catch (error) {
+    console.warn('getPlacesByType: Error fetching places');
+    console.log(error);
+  }
+}
+
+async function getAddress(lat: string, lng: string) {
+  try {
+    const response =
+      await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${GOOGLE_MAPS_API_KEY}
    `);
 
-  return response.data.results[0].formatted_address;
+    return response.data.results[0].formatted_address;
+  } catch (error) {
+    console.warn('getAddress: Error fetching address');
+    console.log(error);
+  }
+}
+
+// TODO: Carry on here.
+async function getDistanceToPlace(placeId: string) {
+  try {
+    const response = await axios.get(
+      `https://maps.googleapis.com/maps/api/distancematrix/json?destinations=${encodeURIComponent(
+        `place_id:${placeId}`
+      )}&origins=${lat},${lng}&key=${GOOGLE_MAPS_API_KEY}`
+    );
+
+    return kmToMiles(response.data.rows[0].elements[0].distance.value);
+    // return response.data.rows[0].elements.distance;
+  } catch (error) {
+    console.warn('getAddress: Error fetching address');
+    console.log(error);
+  }
 }
--- src/screens/Home/CategoriesScreen.tsx
+++ src/screens/Home/CategoriesScreen.tsx
@@ -5,9 +5,10 @@ import {
 } from 'navigation/Navigation.types';
 import { getPlacesByType } from '../../network/GoogleMapsAPI';
 import React, { useEffect, useState } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { ScrollView } from 'react-native';
 import ActivityIndicator from 'components/ActivityIndicator';
 import { Card, Paragraph, Title } from 'react-native-paper';
+import { storeData, getData, removeValue } from '../../storage/storageMethods';
 
 export type CategoriesScreenRoutingProps = StackScreenProps<
   HomeStackParamList,
@@ -16,42 +17,78 @@ export type CategoriesScreenRoutingProps = StackScreenProps<
 
 interface ICategoriesScreenProps extends CategoriesScreenRoutingProps {}
 
-const CategoriesScreen = ({ navigation, route }: ICategoriesScreenProps) => {
+const CategoriesScreen = ({ route }: ICategoriesScreenProps) => {
   const [allPlaces, setAllPlaces] = useState<any>(null);
+  const [, setLoading] = useState(false);
   const { categoryType } = route.params;
-  /* To Do: 
-  - Pass the type into the API
-  - For each result, render a card with the name, distance and accessibility info
-  
-  */
+
+  const getStoredData = async () => {
+    const storedData = await getData();
+    if (!storedData) return null;
+    return storedData;
+  };
 
   useEffect(() => {
+    setLoading(true);
     (async () => {
+      await removeValue();
+
+      // Get data from local storage
+      const storedData = await getStoredData();
+
+      // No stored API data - do a new fetch & store the data
+      if (!storedData) {
+        console.log('1: No stored API data - do a new fetch & store the data');
+        const response = await getPlacesByType(categoryType);
+        setAllPlaces(response);
+        await storeData({
+          categoryType: response,
+        });
+        setLoading(false);
+        return;
+      }
+
+      // Stored API data - check if the current category exists as a property on the object
+      if (storedData.categoryType) {
+        console.log('2: Stored data - exists already: ');
+        setAllPlaces(storedData.categoryType);
+        return;
+      }
+
+      // Stored API data - exists, but not for this category
+      console.log('3: API data exists but not for this category');
       const response = await getPlacesByType(categoryType);
       setAllPlaces(response);
+      const combinedData = {
+        ...storedData,
+        categoryType: response,
+      };
+
+      await storeData(combinedData);
+      setLoading(false);
     })();
   }, []);
 
-  useEffect(() => {
-    console.log('\n****** All places response start *****');
-    console.log(allPlaces?.length);
-    console.log('****** All places response end *****\n');
-  }, [allPlaces]);
   return (
     <ScrollView>
       {!allPlaces ? (
         <ActivityIndicator />
       ) : (
-        allPlaces.map((currentPlace) => {
-          return (
-            <Card>
-              <Card.Content>
-                <Title>{currentPlace.name}</Title>
-                <Paragraph>{currentPlace.location}</Paragraph>
-              </Card.Content>
-            </Card>
-          );
-        })
+        allPlaces
+          .sort(
+            (firstItem, secondItem) => firstItem.distance > secondItem.distance
+          )
+          .map((currentPlace) => {
+            console.log(currentPlace.distance);
+            return (
+              <Card key={`${currentPlace.place_id}`}>
+                <Card.Content>
+                  <Title>{currentPlace.name}</Title>
+                  <Paragraph>{currentPlace.distance} miles away</Paragraph>
+                </Card.Content>
+              </Card>
+            );
+          })
       )}
     </ScrollView>
   );
